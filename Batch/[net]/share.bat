9b7f36470e41179fd6b5c3da8f99b82299d6b0cd
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-02 09:36:02 +0800

    move get-share to tool/network

--- Batch/[net]/share.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-Setlocal enabledelayedexpansion
-::CODER BY xiaoyao9184
-
-net share
-
-pause
---
+++ tool/network/get-share.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+::CODER BY xiaoyao9184
+
+net share
+
+PAUSE
