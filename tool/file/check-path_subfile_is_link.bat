3946fc37f0269c940d932d279af8721973bc51f2
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-02 16:57:05 +0800

    add check-path_subfile_is_link

---
+++ tool/file/check-path_subfile_is_link.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+
+:v
+
+SET echo_path=path for check:
+
+:check
+
+IF "%1"=="" (
+    SET /p work_path=%echo_path%
+) ELSE (
+    SET work_path=%1
+)
+
+ECHO list files for %work_path% 
+FOR /f %%L IN ('DIR /b %work_path%\*.*') DO (
+	ECHO %%L
+
+	FOR %%F IN ("!work_path!\%%L") DO SET attributes=%%~aF
+	IF "!attributes:l=!" NEQ "!attributes!" (
+		ECHO \__ is link
+	)
+)
+
+
+PAUSE
