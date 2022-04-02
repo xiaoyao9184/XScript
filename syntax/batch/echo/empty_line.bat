8e8489bf7107ba877d5bffd0a5c2d0632e2d0ad4
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-02 12:11:53 +0800

    move to syntax/batch

---
+++ syntax/batch/echo/empty_line.bat
@@ -0,0 +1,24 @@
+@REM https://stackoverflow.com/questions/20691060/echo-a-blank-empty-line-to-the-console-from-a-windows-batch-file
+
+
+@echo off
+echo There will be a blank line below
+echo[
+echo Above line is blank
+echo( 
+echo The above line is also blank.
+echo. 
+echo The above line is also blank.
+
+
+echo Also blank below but "echo:" is recommended 
+echo:
+echo(
+echo+
+echo,
+echo/
+echo;
+echo=
+echo[
+echo]
+echo The above line is also blank.
