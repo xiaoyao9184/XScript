0aa6b1e30e74be6eddfcd008b3abb2fcd5b2ada2
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-06 11:31:47 +0800

    add Disable-ExecutionPolicy

---
+++ skill/powershell/execution-policy/Disable-ExecutionPolicy.ps1
@@ -0,0 +1,10 @@
+﻿function Disable-ExecutionPolicy {
+    $obj = (New-Object Management.Automation.AuthorizationManager 'Microsoft.PowerShell')
+    $field = $ExecutionContext.GetType().GetField("_context","nonpublic,instance")
+    $context = $field.GetValue($ExecutionContext)
+    $field = $context.GetType().GetField('_authorizationManager','nonpublic,instance')
+    $field.SetValue($context,$obj)
+}
+Disable-ExecutionPolicy
+
+Write-Host Disable ExecutionPolicy in this session
