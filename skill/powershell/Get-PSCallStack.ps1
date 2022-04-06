c21e4a554add26c648385120fb47109b6daa2ad6
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-06 11:23:23 +0800

    add Get-PSCallStack

---
+++ skill/powershell/Get-PSCallStack.ps1
@@ -0,0 +1,21 @@
+﻿
+function List-PSCallStack($not) {
+    $CallStacks = Get-PSCallStack | Where-Object  {$_.Command -ne $not}
+    return $CallStacks
+}
+
+$CallStacks = List-PSCallStack("<ScriptBlock>")
+
+Write-Host ($CallStacks | Format-Table | Out-String)
+
+Write-Host CallStack ScriptName Count: $CallStacks.Count
+foreach($CallStack in $CallStacks){
+    Write-Host CallStack ScriptName: $CallStack.ScriptName
+}
+
+Write-Host MyCommand: $MyInvocation.MyCommand.Definition
+
+if($CallStack[0].ScriptName -in $MyInvocation.MyCommand.Definition){
+    Write-Host CallStack and MyInvocation in same file
+    Pause
+}
