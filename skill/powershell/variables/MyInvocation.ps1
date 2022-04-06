00d75a540172e98690744faf596600f2fec43fa8
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-06 11:23:44 +0800

    add MyInvocation

---
+++ skill/powershell/variables/MyInvocation.ps1
@@ -0,0 +1,7 @@
+﻿Write-Output "list MyInvocation"
+Write-Host ($MyInvocation | Format-List | Out-String)
+
+
+Pause
+Write-Output "list MyInvocation by PSobject"
+Write-Host ($MyInvocation.PSobject.Properties | Format-List | Out-String)
